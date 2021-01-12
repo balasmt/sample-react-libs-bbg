a99131c817cef20d04c71855d8a2df302e350118
balasmt <Bala.Bugatha@siliconmtn.com>
2021-01-12 14:39:37 -0700

    InitialCommit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
-
-import { ExampleComponent } from 'color-heading'
-import 'color-heading/dist/index.css'
-
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+import React,{ Component} from 'react';
+import { ColorHeadingOne }  from 'color-heading';
+import { ColorHeadingTwo }  from 'color-heading';
+export default class App extends Component{
+  render(){
+    return(
+      <div>
+        <ColorHeadingOne text="colored heading one is working" color={"red"}/>
+        <ColorHeadingTwo text="colored heading two is working" color={"blue"}/>
+      </div>
+    );
+  }
 }
-
-export default App
